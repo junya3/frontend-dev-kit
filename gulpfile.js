e99b06518f07cf4e6a2151078dbe2f5b98685208
junya3 <obara.junya0728@gmail.com>
2025-09-02 15:45:56 +0900

    update path

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// gulpfile.js
 const { series, parallel, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
 
@@ -5,31 +6,64 @@ const browserSync = require('browser-sync').create();
 const { compilePugDist, compilePugPreview } = require('./gulp/pug');
 const { compileSassDist, compileSassPreview } = require('./gulp/sass');
 const { compileTsDist, compileTsPreview } = require('./gulp/ts');
+// const { imagesPreview, imagesDist } = require('./gulp/images');
 
 // preview ディレクトリを変数で定義
 const paths = {
   preview: 'preview',
   pug: 'src/pug/**/*.pug',
   scss: 'src/scss/**/*.scss',
-  ts: 'src/scss/**/*.ts',
+  ts: 'src/ts/**/*.ts',
+  images: 'src/assets/images/**/*',
 };
 
 // watch + browserSync
 function serve() {
   browserSync.init({
-    server: { baseDir: paths.preview }, // ← ここを変数に変更
+    server: { baseDir: paths.preview },
   });
 
   watch(paths.pug, compilePugPreview).on('change', browserSync.reload);
-  watch(paths.scss, series(compileSassPreview, browserSync.reload));
-  watch('src/ts/**/*.ts', compileTsPreview).on('change', browserSync.reload);
+  watch(
+    paths.scss,
+    series(compileSassPreview, (done) => {
+      browserSync.reload();
+      done();
+    })
+  );
+  watch(
+    paths.ts,
+    series(compileTsPreview, (done) => {
+      browserSync.reload();
+      done();
+    })
+  );
+  // watch(paths.images, series(imagesPreview, (done) => {
+  //   browserSync.reload();
+  //   done();
+  // }));
 }
 
 // exports でタスク公開
-exports.build = parallel(compilePugDist, compileSassDist, compileTsDist);
-exports.preview = series(
-  parallel(compilePugPreview, compileSassPreview, compileTsPreview),
+const build = parallel(
+  compilePugDist,
+  compileSassDist,
+  compileTsDist
+  // imagesDist
+);
+
+const preview = series(
+  parallel(
+    compilePugPreview,
+    compileSassPreview,
+    compileTsPreview
+    // imagesPreview
+  ),
   serve
 );
 
-exports.watch = exports.preview;
+module.exports = {
+  build,
+  preview,
+  watch: preview,
+};
